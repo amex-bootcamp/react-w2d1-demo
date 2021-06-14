073b5c1552e64062f45d8a69741961d80265ac0d
kchia <kchia87@gmail.com>
2021-06-14 11:29:34 -0400

    import-export App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import App from './App.js'; // .js is optional 
 import App from './App';
 
 ReactDOM.render(
