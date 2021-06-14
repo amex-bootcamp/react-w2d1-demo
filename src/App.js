3c89831dd659b747b6b44108f949ed4016d7e057
kchia <kchia87@gmail.com>
2021-06-14 14:10:05 -0400

    add a note about js extension

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Welcome from "./components/Welcome/Welcome";
 import Person from "./components/Person/Person";
 import Counter from "./components/Counter/Counter";
+// import Counter from "./components/Counter/Counter.js"; // this will work too
+// import "./components/Counter/Counter.css"
 
 export default class App extends Component {
   render() {
