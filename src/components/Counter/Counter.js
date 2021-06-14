39852ff8177b9cf162dc957bce50e662e15f885c
kchia <kchia87@gmail.com>
2021-06-14 14:03:12 -0400

    set up Counter component with static jsx

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  render() {
+    return (
+      <div>
+        Counter
+        <h2>0</h2>
+      </div>
+    )
+  }
+}
