4db2a4c1cf6a518671c48dc086cb57eadde1fc9e
kchia <kchia87@gmail.com>
2021-06-14 14:19:08 -0400

    add count state to the Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import "./Counter.css";
 
 export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 10
+    }
+  }
+
   render() {
     return (
       <div className="Counter-container">
         Counter
-        <h2>0</h2>
+        <h2>{this.state.count}</h2>
       </div>
     )
   }
