10cfcbb524e309554da62362a7a5e89788e3608d
kchia <kchia87@gmail.com>
2021-06-14 11:39:34 -0400

    add Welcome component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
+import Welcome from "./components/Welcome/Welcome";
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
         My App
+        <Welcome />
       </div>
     )
   }
 }
-
-export default App;
