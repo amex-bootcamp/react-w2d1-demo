e44953fc35d52f5db7cbc006fbc013e033c103e4
kchia <kchia87@gmail.com>
2021-06-15 14:46:28 -0400

    refactor Person to use a functional component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,11 +1,21 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export default class Person extends Component {
-  render() {
-    return (
-      <div>
-        Hello, {this.props.firstName} {this.props.lastName}
-      </div>
-    );
-  }
+// export default class Person extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello, {this.props.firstName} {this.props.lastName}
+//       </div>
+//     );
+//   }
+// }
+
+import React from "react";
+
+export default function Person(props) {
+  return (
+    <div>
+      Hello, {props.firstName} {props.lastName}
+    </div>
+  );
 }
