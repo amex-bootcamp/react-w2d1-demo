9c025e9b5b0d98293f14782ef1792414beee23f9
kchia <kchia87@gmail.com>
2021-06-15 10:55:31 -0400

    add static ContactForm

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ContactForm extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
       firstName: "Hou",
       lastName: "Chia",
-      email:"houchia@gmail.com",
-      message: "message"     
-    }
+      email: "houchia@gmail.com",
+      message: "message",
+    };
   }
-  
+
   render() {
     return (
       <div>
         <h1>Hou's Contact Form</h1>
-        <p>{this.state.firstName}</p>
-        <p>{this.state.lastName}</p>
-        <p>{this.state.email}</p>
-        <p>{this.state.message}</p>
         <form>
+          <ul>
+            <li>
+              <label htmlFor="firstName">First Name:</label>
+              <input type="text" id="firstName" name="firstName" />
+            </li>
+            <li>
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" id="lastName" name="lastName" />
+            </li>
+            <li>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
+            </li>
+            <li>
+              <label htmlFor="message">Message:</label>
+              <input type="text" id="message" name="message" />
+            </li>
+            <button>Send contact form</button>
+          </ul>
         </form>
       </div>
-    )
+    );
   }
 }
